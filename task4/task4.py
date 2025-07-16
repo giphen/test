aced47d34927d7bc95056ed9e698d55ba25a9fd4
giphen.hpcr <giphen.hpcr@gmail.com>
2025-07-16 12:51:51 +0400

    update

--- task4/task4.py
+++ task4/task4.py
@@ -3,16 +3,20 @@ import sys
 def min_moves(numbers_file):
 
     with open(numbers_file, 'r') as file:
-        nums = [int(line.strip()) for line in file if line.strip()]
+        nums = [int(line.strip()) for line in file]
     
     
     nums.sort()
-    median = nums[len(nums)//2] 
-    return sum(abs(num - median) for num in nums)
+    median = nums[len(nums) // 2] 
+    total_moves = 0
+    for num in nums:
+        if num > median:
+            total_moves += num - median
+        else:
+            total_moves += median - num
+    return total_moves
 
-if __name__ == "__main__":
-
-    moves = min_moves(sys.argv[1])
-    print(moves)
+score = min_moves(sys.argv[1])
+print(score)
 
 # Для использования: python task4.py numbers.txt
